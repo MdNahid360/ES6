2cc3e4cdbdc9580d5c7e6c31d8a2ecba159f597f
MdNahid360 <mdnahid360s@gmail.com>
2021-02-02 03:26:58 -0800

    this is nul vs undefine

---
+++ modiol 23/null_vs_undifine.js
@@ -0,0 +1,23 @@
+//undefine[
+  //  1) without return
+//]
+ 
+//const nahid ;
+function lolo(num1, num2){
+    console.log(num1, num2);
+   // return lolo;
+}
+
+const ans = lolo(11, 88);
+console.log(ans);
+  
+///undefine
+
+function lolo(num1, num2){
+    console.log(num1, num2);
+   // return lolo;
+}
+
+const undefine = lolo(11, 88);
+console.log(undefine);
+  
