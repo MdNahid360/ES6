df35a0efc5e0e0ceac86ebd828c05cad99987ce3
MdNahid360 <mdnahid360s@gmail.com>
2021-02-02 01:45:04 -0800

    modiul true vs false

---
+++ modiol 23/true-falsy.js
@@ -0,0 +1,18 @@
+//true : []
+//true : " "
+
+
+//////////////////
+//falsy : NaN
+//falsy : valuen jodi nathake
+//falsy : ''
+//falsy : value = 0;
+//falsy : nul
+let ans = [];
+console.log(ans);
+if(ans){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
