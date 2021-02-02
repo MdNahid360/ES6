829a61715dbfa54a9204bfcc9ce1daf9d94b132c
MdNahid360 <mdnahid360s@gmail.com>
2021-02-02 04:39:29 -0800

    duble vs threple equal

---
+++ modiol 23/duble-vs-equal.js
@@ -0,0 +1,21 @@
+const num1 = 1;
+const num2 = false;
+if(null == num2){
+    console.log('this is true');
+}
+else{
+    console.log('this is false');
+}
+//////////////////////////////////
+//0 = false
+//1 = true
+
+const num3 = 0;
+const num4 = false;
+if(num3 === num4){
+    console.log('this is true');
+}
+else{
+    console.log('this is false');
+}
+ 
